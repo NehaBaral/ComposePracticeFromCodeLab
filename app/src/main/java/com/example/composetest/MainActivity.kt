8f4c12394915c4796e30f3e50a15fa273a79c1e4
NehaBaral <nehabaral011@gmail.com>
2022-03-14 15:06:53 +0545

    Compose Practice from codelab

---
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.example.composetest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composetest.ui.BasicsCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsCodelabTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    AppData()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun AppData() {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+    //   Greetings()
+    if (shouldShowOnboarding) {
+        onBoardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun MessageCard() {
+    Spacer(modifier = Modifier.width(8.dp))
+    Column(modifier = Modifier.padding(8.dp)) {
+        val itemList: List<String> = listOf("Chandler", "Racheal", "Joey")
+        for (item in itemList) {
+            printMessage(message = item)
+        }
+    }
+}
+
+@Composable
+fun Greetings(names: List<String> = List(100) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            printMessage(name)
+        }
+    }
+}
+
+@Composable
+fun printMessage(message: String) {
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        var expanded by remember { mutableStateOf(false) }
+        val extraPadding by animateDpAsState(
+            if (expanded) 48.dp else 0.dp,
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy,
+                stiffness = Spring.StiffnessLow
+            )
+        )
+        Row(Modifier.padding(24.dp)) {
+            Image(
+                painter = painterResource(R.drawable.ic_snacks_icon),
+                contentDescription = "Profile Photo",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(shape = CircleShape)
+            )
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(12.dp)
+            ) {
+                Text(text = "Hello")
+                Text(
+                    text = "${message}", style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                if (expanded) {
+                    Text(
+                        text = ("Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy. ").repeat(4),
+                    )
+                }
+            }
+
+            OutlinedButton(onClick = {
+                expanded = !expanded
+            }) {
+              //  Icon(contentDescription = if (expanded) stringResource(R.string.show_more) else stringResource(R.string.show_less))
+                if (expanded) Icons.Filled
+                else Text(text = "Show more")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 340)
+@Composable
+fun PreviewMessage() {
+    MessageCard()
+}
+
+@Composable
+fun onBoardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome to JetPack Compose")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text(text = "Continue")
+            }
+        }
+    }
+}
+
